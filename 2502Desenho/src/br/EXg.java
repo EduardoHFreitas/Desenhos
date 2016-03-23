acdfd59b56395c791da2a0d4c081b70b54cbbcf7
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-03-22 22:01:44 -0300

    Commit classes dos exercicios "E" ao "H"

---
+++ 2502Desenho/src/br/EXg.java
@@ -0,0 +1,21 @@
+package br;
+
+public class EXg extends ClasseAbstrata {
+
+	@Override
+	public void Desenhar(int SIZE) {
+		for (int i = 0; i < SIZE; i++){
+			for (int h = 0; h < SIZE; h++){
+				if (i == 0 || i == SIZE - 1)
+					System.out.print("# ");
+				else {
+					if (h == (SIZE - (i + 1)))
+						System.out.print("# ");
+					else 
+						System.out.print("  ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+}
